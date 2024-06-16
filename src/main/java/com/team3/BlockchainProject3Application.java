adc7f430a1775b6ad3f3a8d97d079040dc70fcc2
thesonofwind99 <dunhan999@gmail.com>
2024-06-16 10:58:37 +0700

    first commit

---
+++ src/main/java/com/team3/BlockchainProject3Application.java
@@ -0,0 +1,13 @@
+package com.team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainProject3Application.class, args);
+	}
+
+}
